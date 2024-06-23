bbfbc6fb3268f22e498108fb3d2ddb52f901e877
Thomas Bärwinkel <thomas.baerwinkel@quasiris.de>
2024-06-23 19:32:51 +0200

    add aws-bedrock-client

---
+++ aws-bedrock-client/src/main/java/de/tblsoft/aws/bedrock/client/AwsBedrockClientApplication.java
@@ -0,0 +1,13 @@
+package de.tblsoft.aws.bedrock.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBedrockClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBedrockClientApplication.class, args);
+	}
+
+}
---
+++ aws-bedrock-client/src/test/java/de/tblsoft/aws/bedrock/client/AwsBedrockClientApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tblsoft.aws.bedrock.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBedrockClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
