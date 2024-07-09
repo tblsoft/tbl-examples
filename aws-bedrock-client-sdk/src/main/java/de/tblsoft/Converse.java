8d50c3fb997238259d1dc01cb1b924407dd58d58
Thomas Bärwinkel <thomas.baerwinkel@quasiris.de>
2024-07-10 01:44:55 +0200

    add aws-bedrock-client-sdk

---
+++ aws-bedrock-client-sdk/src/main/java/de/tblsoft/Converse.java
@@ -0,0 +1,71 @@
+package de.tblsoft;
+
+
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
+import software.amazon.awssdk.services.bedrockruntime.model.ContentBlock;
+import software.amazon.awssdk.services.bedrockruntime.model.ConversationRole;
+import software.amazon.awssdk.services.bedrockruntime.model.ConverseResponse;
+import software.amazon.awssdk.services.bedrockruntime.model.Message;
+
+public class Converse {
+
+    public static String converse() {
+
+        // Create a Bedrock Runtime client in the AWS Region you want to use.
+        // Replace the DefaultCredentialsProvider with your preferred credentials provider.
+
+        AwsSessionCredentials credentials = AwsSessionCredentials.builder().
+                accessKeyId("****").
+                secretAccessKey("*****").
+                sessionToken("*******").
+                build();
+        StaticCredentialsProvider provider = StaticCredentialsProvider.create(credentials);
+
+        var client = BedrockRuntimeClient.builder()
+                .credentialsProvider(provider)
+                .region(Region.US_EAST_1)
+                .build();
+
+        // Set the model ID, e.g., Claude 3 Haiku.
+        var modelId = "anthropic.claude-3-haiku-20240307-v1:0";
+
+        // Create the input text and embed it in a message object with the user role.
+        var inputText = "Describe the purpose of a 'hello world' program in one line.";
+        var message = Message.builder()
+                .content(ContentBlock.fromText(inputText))
+                .role(ConversationRole.USER)
+                .build();
+
+
+        try {
+            // Send the message with a basic inference configuration.
+            ConverseResponse response = client.converse(request -> request
+                    .modelId(modelId)
+                    .messages(message)
+                    .inferenceConfig(config -> config
+                            .maxTokens(512)
+                            .temperature(0.5F)
+                            .topP(0.9F)));
+
+            // Retrieve the generated text from Bedrock's response object.
+            var responseText = response.output().message().content().get(0).text();
+            System.out.println(responseText);
+
+            return responseText;
+
+        } catch (SdkClientException e) {
+            System.err.printf("ERROR: Can't invoke '%s'. Reason: %s", modelId, e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        converse();
+    }
+}
+// snippet-end:[bedrock-runtime.java2.Converse_AnthropicClaude]
+
